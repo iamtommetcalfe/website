b56f83119dda2c44c2d0ba44c1286233bcb2e432
iamtommetcalfe <iamtommetcalfe@gmail.com>
2024-06-28 16:47:30 +0100

    added publicPath for GH action deploy

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const basePath = process.env.NODE_ENV === 'production'
+    ? '/website/'
+    : '/';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: basePath,
   plugins: [
       vue(),
       VitePWA()
