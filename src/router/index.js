ac31a15a4dbbc246d6b2d404b00784dddbec6ec7
iamtommetcalfe <iamtommetcalfe@gmail.com>
2025-06-23 20:18:41 +0100

    migrated to history based routing instead of hash based

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Homepage from '../views/Homepage.vue';
 import NotFound from '../views/NotFound.vue';
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
