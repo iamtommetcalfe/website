9cda04837ba165c6b3b4e8637e7106eddf134f4d
Tom Stirrop-Metcalfe <iamtommetcalfe@gmail.com>
2025-08-20 22:19:46 +0100

    Update Homepage.vue

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -52,7 +52,7 @@ import { useSeo } from '@/composables/useSeo';
 
 // Set SEO metadata for the Homepage
 useSeo({
-  title: 'Tom Stirrop-Metcalfe | Software Engineering Manager | Birmingham',
+  title: 'Homagepage | Tom Stirrop-Metcalfe | Engineering Leader',
   description:
     'My name is Tom Stirrop-Metcalfe. I am a passionate and driven engineering leader who wants to enable others to realise their full potential.',
   canonicalUrl: 'https://iamtommetcalfe.com/',
